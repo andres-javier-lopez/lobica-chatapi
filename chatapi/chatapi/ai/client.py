da19a8095a71bb6f20c82db40434a7a04d97ecb8
Andres Javier Lopez <code@andresjavierlopez.com>
2023-12-04 15:55:44 -0600

    Add additional parameters to client

--- chatapi/chatapi/ai/client.py
+++ chatapi/chatapi/ai/client.py
@@ -1,6 +1,10 @@
 from langchain.chat_models import ChatOpenAI
 
-from chatapi.settings import OPENAI_API_KEY
+from chatapi.settings import OPENAI_API_KEY, OPENAI_MODEL, OPENAI_TEMPERATURE
 
 
-chat_model = ChatOpenAI(api_key=OPENAI_API_KEY)
+chat_model = ChatOpenAI(
+    api_key=OPENAI_API_KEY,
+    model=OPENAI_MODEL,
+    temperature=OPENAI_TEMPERATURE,
+)
